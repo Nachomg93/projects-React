8861362502112321d4bff0d23895cbab3d0b4ec6
nacho <nachomg93@gmail.com>
2022-11-29 23:05:27 +0100

    Terminado el segundo proyecto

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import Boton from "./componentes/Boton";
 import Contador from "./componentes/Contador";
 import freeCodeCampLogo from "./imagenes/freecodecamp-logo.jpg";
+import { useState } from "react";
 
 function App() {
+  const [numClics, setNumClics] = useState(0);
 
   const manejarClic = () => {
-    console.log('Clic');
-  }
+    setNumClics(numClics + 1);
+  };
 
   const reiniciarContador = () => {
-    console.log('Reiniciar');
-  }
+    setNumClics(0);
+  };
 
   return (
     <div className="App">
@@ -23,15 +25,13 @@ function App() {
         />
       </div>
       <div className="contenedor-principal">
-        <Contador numClics='5'/>
-        <Boton 
-        texto='Clic'
-        esBotonDeClic={true}
-        manejarClic={manejarClic}/>
-        <Boton 
-        texto='Reiniciar'
-        esBotonDeClic={false}
-        manejarClic={reiniciarContador}/>
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
       </div>
     </div>
   );
